2fa8011420b0b4c99381eb3b028271e0705145cc
Soulsender <nexushopper3000@gmail.com>
2022-04-05 11:43:15 -0700

    fixed script

--- alfa-AWUS036ACH-drivers/alfa-drivers.sh
+++ alfa-AWUS036ACH-drivers/alfa-drivers.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo "By Soulsender"
-echo "Commands courtesy of David Bombal"
-echo "https://www.youtube.com/watch?v=hEXwOkyYNL0&t=303s"
+sleep 3
 
 # update repositories
 apt-get -y update
-apt-get -y upgrade 
-
-# install drivers (chipset rtl8812au)
-apt-get -y install realtek-rtl88xxau-dkms
 
 # install dkms
 apt-get -y install dkms
 
-# ======
-# sometimes this will work but the second part of the script will run anyways to make sure
-# ======
+# install drivers (chipset rtl8812au)
+apt-get -y install realtek-rtl88xxau-dkms
 
 # install git if not installed
 apt-get -y install git
 
 # clone more drivers
+cd ~
 git clone https://github.com/aircrack-ng/rtl8812au
 
 # compile
 cd ~/rtl8812au
 make
 make install
+rm -rf ~/rtl8812au
 
 # may need to unplug and replug the adapter
-echo "The script is finished. Use iwconfig to check (probably listed as wlan0). If it is still not listed, unplug and replug the adapter. 
+echo ""
+echo "The script is finished. Use iwconfig to check (the adapter is probably listed as wlan0). If it is still not listed, unplug and replug the adapter. 
 If you are running this via virtual machine, you may also need to check your hypervisor (typically VMware or Virtualbox) 
 settings and see if the USB port is being registered."
+sleep 2
