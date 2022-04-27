7fc2ae740bb3e2d89aadf802509116eb7fb76fff
Soulsender <nexushopper3000@gmail.com>
2022-04-27 15:37:12 -0700

    fixed faulty driver installation

--- alfa-AWUS036ACH-drivers/alfa-drivers.sh
+++ alfa-AWUS036ACH-drivers/alfa-drivers.sh
@@ -27,26 +27,71 @@ apt-get -y install realtek-rtl88xxau-dkms
 
 if [ $? -ne 1 ]
 then
-  # install git if not installed
-  apt-get -y install git
-  echo -e "${red} Uh oh, it seems there was a fucky wucky and the repository isn't found which means you probably arent running this on kali. Installing via sketchy reddit thread protocol initiated.${reset}"
-  sleep 8
-  # instructions from
-  # https://www.reddit.com/r/Ubuntu/comments/gbb1nw/realtek_rtl8812au_netis_wf2190_usb_wireless/
-  dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt -y purge
-  sudo apt install build-essential git
-  cd ~
-  git clone https://github.com/gnab/rtl8812au.git
-  cd ~/rtl8812au
-  make
-  sudo make install
-  sudo modprobe 8812au
-  rm -rf ~/rtl8812au
+  echo -e "${red}Uh oh, it seems there was a fucky wucky and the driver repositories aren't found which means you probably aren't running this script on Kali. The only way I've found to set this up properly is to add the Kali repositories to your system sources.${reset}"
+
+  while true
+  do
+      read -r -p "Would you like to add Kali repositories to your system? [Y/n] " input
+ 
+      case $input in
+            [yY][eE][sS]|[yY])
+
+                  while true
+                  do
+                        read -r -p "Would you like to create a backup of sources.list in the home directory? [Y/n] " input
+                        case $input in
+                              [yY][eE][sS]|[yY])
+                                    echo -e "${green}Backing up sources.list ${reset}"
+                                    sleep 3
+                                    sudo cp -R /etc/apt/sources.list ~/
+                                    echo -e "Sources backed up in ~"
+                                    sleep 3
+                                    break
+                                    ;;
+                              [nN][oO]|[nN])
+                                    echo -e "${red}Operation canceled.${reset}"
+                                    break
+                                    ;;
+                              *)
+                                    echo -e "${red}Invalid input...${reset}"
+                                    ;;
+                        esac      
+                  done
+
+                  echo -e "${green}Adding Kali Repositories"
+                  sleep 3
+                  sudo tee -a /etc/apt/sources.list<<EOF
+deb http://http.kali.org/kali kali-rolling main non-free contrib
+deb-src http://http.kali.org/kali kali-rolling main non-free contrib
+EOF
+                  break
+                  ;;
+            [nN][oO]|[nN])
+                  echo -e "${red}Operation canceled.${reset}"
+                  exit
+                  break
+                  ;;
+            *)
+                  echo -e "${red}Invalid input...${reset}"
+                  ;;
+      esac      
+done
 fi
+
+apt-get -y install dkms
+apt-get -y install realtek-rtl88xxau-dkms
+apt-get -y install rtl8812au-dkms
+
+# clone and compile
+cd ~
+git clone https://github.com/aircrack-ng/rtl8812au
+cd ~/rtl8812au
+make
+make install
 
 # may need to unplug and replug the adapter
 echo ""
 echo -e "${green}The script is finished. Use iwconfig to check (the adapter is probably listed as wlan0). If it is still not listed, unplug and replug the adapter. 
 If you are running this via virtual machine, you may also need to check your hypervisor (typically VMware or Virtualbox) 
 settings and see if the USB port is being registered.${reset}"
-sleep 2
+sleep 3
