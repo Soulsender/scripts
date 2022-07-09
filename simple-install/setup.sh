f9e7050517c80355a85606a7949bffe71ba86f0e
Soulsender <83854400+Soulsender@users.noreply.github.com>
2022-07-08 20:40:58 -0700

    Update setup.sh

--- simple-install/setup.sh
+++ simple-install/setup.sh
@@ -11,13 +11,8 @@ apt-get -y upgrade
 # installs
 sudo apt-get install wireguard proxychains tor macchanger wireshark qbittorrent network-manager code flatpak chromium sublime-text kde-plasma-desktop vlc mpv obs flameshot cmatrix hollywood terminator virtualbox  -y
 
-# librewolf
-distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
-echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list
-sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
-sudo apt update
-sudo apt install librewolf -y
-
 # flatpaks
 sudo flatpak install discord
 
+echo -e "${GRN}Script complete."
+echo -e "${BLU}Install LibreWolf"
