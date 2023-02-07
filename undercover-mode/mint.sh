4394504c13c07ccd6c0dfd98fc960985ea86188c
Soulsender <soulsender@soulsender.me>
2023-02-07 12:15:25 -0800

    undercover kali mode

---
+++ undercover-mode/mint.sh
@@ -0,0 +1,6 @@
+# Set to windows
+gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Red'
+gsettings set org.cinnamon panels-enabled "['1:0:top']" 
+gsettings set org.cinnamon.desktop.interface icon-theme 'BeautyLine'
+gsettings set org.cinnamon.desktop.wm.preferences theme 'Mint-Y-Dark-Red'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Red'
---
+++ undercover-mode/windows.sh
@@ -0,0 +1,6 @@
+# Set to windows
+gsettings set org.cinnamon.theme name 'Windows-10-Dark-3.2.1-dark'
+gsettings set org.cinnamon panels-enabled "['1:0:bottom']" 
+gsettings set org.cinnamon.desktop.interface icon-theme 'Windows-10-master'
+gsettings set org.cinnamon.desktop.wm.preferences theme 'Windows-10-Dark-3.2.1-dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme "Windows-10-Dark-3.2.1-dark"
