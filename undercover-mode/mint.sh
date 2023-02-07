b1c104de89d543fc7f8fb5f147bdd834a0afa77a
Soulsender <soulsender@soulsender.me>
2023-02-07 14:15:57 -0800

    update for logo changing

--- undercover-mode/mint.sh
+++ undercover-mode/mint.sh
@@ -4,3 +4,5 @@ gsettings set org.cinnamon panels-enabled "['1:0:top']"
 gsettings set org.cinnamon.desktop.interface icon-theme 'BeautyLine'
 gsettings set org.cinnamon.desktop.wm.preferences theme 'Mint-Y-Dark-Red'
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Red'
+sed -i 's/windows10logo.png/favicon_dark.svg/g' /home/soul/.config/cinnamon/spices/menu@cinnamon.org/0.json
+sed -i 's/"value": ""/"value": "Soul"/g' /home/soul/.config/cinnamon/spices/menu@cinnamon.org/0.json
--- undercover-mode/windows.sh
+++ undercover-mode/windows.sh
@@ -4,3 +4,5 @@ gsettings set org.cinnamon panels-enabled "['1:0:bottom']"
 gsettings set org.cinnamon.desktop.interface icon-theme 'Windows-10-master'
 gsettings set org.cinnamon.desktop.wm.preferences theme 'Windows-10-Dark-3.2.1-dark'
 gsettings set org.cinnamon.desktop.interface gtk-theme "Windows-10-Dark-3.2.1-dark"
+sed -i 's/favicon_dark.svg/windows10logo.png/g' /home/soul/.config/cinnamon/spices/menu@cinnamon.org/0.json
+sed -i 's/"value": "Soul"/"value": ""/g' /home/soul/.config/cinnamon/spices/menu@cinnamon.org/0.json
