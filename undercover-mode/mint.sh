0210817bb4754023b5810754ff8b3a7579b768ff
Soulsender <soulsender@soulsender.me>
2023-02-07 15:03:47 -0800

    fixed comment

--- undercover-mode/mint.sh
+++ undercover-mode/mint.sh
@@ -1,4 +1,4 @@
-# Set to windows
+# Set to mint
 gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Red'
 gsettings set org.cinnamon panels-enabled "['1:0:top']" 
 gsettings set org.cinnamon.desktop.interface icon-theme 'BeautyLine'
